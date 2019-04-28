d80fde0fafcbdc38ff8c18e0d4917f33747a0473
Kaelin Hooper <kaelin@Kaelins-MacBook-Pro.local>
2019-04-28 15:08:56 -0500

    Fixed bug in Mass class unitDistance method

--- universe.js
+++ universe.js
@@ -16,11 +16,10 @@ class Mass {
   draw() {
     circle((this.pos.x * scale) + (dim / 2),
            (this.pos.y * scale) + (dim / 2),
-           (this.radius * scale) * 300000);
+           this.radius * scale * 100);
   }
 
   updateForce() {
-    print(this.name);
     this.accel.x = 0;
     this.accel.y = 0;
 
@@ -40,15 +39,15 @@ class Mass {
       this.accel.y += force_y / this.mass
     }
 
-    print(this.vel.x);
-    print(this.vel.y);
+    // print(`${this.name} x: ${this.vel.x}`);
+    // print(`${this.name} y: ${this.vel.x}`);
     this.vel.x += this.accel.x * delta_t;
     this.vel.y += this.accel.y * delta_t;
   }
 
   updatePosition() {
-    this.pos.x += this.vel.x*delta_t + 0.5 * this.accel.x * Math.pow(delta_t, 2);
-    this.pos.y += this.vel.y*delta_t + 0.5 * this.accel.y * Math.pow(delta_t, 2);
+    this.pos.x += this.vel.x*delta_t; // + 0.5 * this.accel.x * Math.pow(delta_t, 2);
+    this.pos.y += this.vel.y*delta_t; // + 0.5 * this.accel.y * Math.pow(delta_t, 2);
   }
 
   distance(other) {
@@ -56,8 +55,8 @@ class Mass {
   }
 
   unitDistance(other) {
-    return [this.pos.x - other.pos.x / this.distance(other),
-            this.pos.y - other.pos.y / this.distance(other)];
+    return [(this.pos.x - other.pos.x) / this.distance(other),
+            (this.pos.y - other.pos.y) / this.distance(other)];
   }
 }
 
@@ -66,14 +65,16 @@ class Mass {
 let t = 0;
 let fr = 60;
 const scale = 1 / Math.pow(10, 9);  // in meters
-let delta_t = 1 / 30;
-const G = 6.674 * Math.pow(10, -11);
+let delta_t = 86140;
+const G = 6.67408 * Math.pow(10, -11);
 const dim = 1000;
 let universe = [
-  HelloWorld = new Mass("Earth", 0, -152 * Math.pow(10, 9), 3958.8, 5.972 * Math.pow(10, 24)),
-  BrendanWorld = new Mass("Sun", 0, 0, 432170, 1.989 * Math.pow(10, 30))
+  Sun = new Mass("Sun", 0, 0, 695.51 * Math.pow(10, 6), 1.989 * Math.pow(10, 30)),
+  Earth = new Mass("Earth", 0, -152 * Math.pow(10, 9), 6.371 * Math.pow(10, 6), 5.972 * Math.pow(10, 24)),
+  Mars = new Mass("Mars", 0, -227.9 * Math.pow(10, 9), 3.3895 * Math.pow(10, 6), 6.39 * Math.pow(10, 23))
 ];
-HelloWorld.vel.x = 30000 * 86400;
+Mars.vel.x = 24000;
+Earth.vel.x = 30000;
 
 
 
@@ -86,11 +87,11 @@ function setup() {
 
 function draw() {
   background(230);
-  HelloWorld.draw();
-  // DavidWorld.draw();
-  BrendanWorld.draw();
+  Mars.draw();
+  Earth.draw();
+  Sun.draw();
 
-  print("\nTime: " + t);
+  print(`Days: ${t / 86140}`);
   t += delta_t;
 
   // Update forces
