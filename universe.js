c84db2afabd3c871e91336cee99f52a7af62820a
Kaelin <ka3lin@gmail.com>
2019-06-07 01:26:06 -0500

    Added grid point feature that warps to indicate graviational pull

--- universe.js
+++ universe.js
@@ -3,30 +3,28 @@ ether = new p5();
 
 
 class Mass {
-  constructor(name, x, y, radius, mass) {
-      this.name = name;
-      this.mass = mass;
-      this.radius = radius;
-      this.origin = createVector(x, y);
-      this.pos = createVector(x, y);
-      this.vel = createVector(0, 0);
-      this.accel = createVector(0, 0);
+  constructor(name, x, y, radius, mass, isGridDot = false) {
+      this.name = name
+      this.mass = mass
+      this.radius = radius
+      this.pos = createVector(x, y)
+      this.vel = createVector(0, 0)
+      this.accel = createVector(0, 0)
+      this.isGridDot = isGridDot
   }
 
-  draw(displayText = false) {
-    let scaledXpos = this.pos.x * scale + dim / 2;
-    let scaledYpos = this.pos.y * scale + dim / 2;
-    let scaledRadius = this.radius * scale * 1000;
+  draw() {
+    let scaledXpos = this.pos.x * scale + dim / 2
+    let scaledYpos = this.pos.y * scale + dim / 2
+    let scaledRadius = this.radius * scale * 1000
 
-    circle(scaledXpos,
-           scaledYpos,
-           scaledRadius);
-
-    if (displayText) {
-      textSize(8);
-      text(this.name,
-        scaledXpos - this.name.length * 2,
-        scaledYpos + scaledRadius + 5);
+    if (this.isGridDot) {
+      point(scaledXpos, scaledYpos)
+      stroke(0)
+    } else {
+      circle(scaledXpos,
+             scaledYpos,
+             scaledRadius);
     }
   }
 
@@ -36,29 +34,27 @@ class Mass {
 
     var i;
     for (i = 0; i < universe.length; i++) {
-      let obj = universe[i];
-      if (this == obj) { continue; }
+      let obj = universe[i]
+      if (this == obj) { continue }
 
-      let m1 = this.mass;
-      let m2 = obj.mass;
-      let r = this.distance(obj);
-      let force = G*m1*m2 / Math.pow(r, 2);
-      let r_hat = this.unitDistance(obj);
-      let force_x = force * -r_hat[0];
-      let force_y = force * -r_hat[1];
+      let m1 = this.mass
+      let m2 = obj.mass
+      let r = this.distance(obj)
+      let force = G*m1*m2 / Math.pow(r, 2)
+      let r_hat = this.unitDistance(obj)
+      let force_x = force * -r_hat[0]
+      let force_y = force * -r_hat[1]
       this.accel.x += force_x / this.mass
       this.accel.y += force_y / this.mass
     }
 
-    // print(`${this.name} x: ${this.vel.x}`);
-    // print(`${this.name} y: ${this.vel.x}`);
-    this.vel.x += this.accel.x * delta_t;
-    this.vel.y += this.accel.y * delta_t;
+    this.vel.x += this.accel.x * delta_t
+    this.vel.y += this.accel.y * delta_t
   }
 
   updatePosition() {
-    this.pos.x += this.vel.x*delta_t; // + 0.5 * this.accel.x * Math.pow(delta_t, 2);
-    this.pos.y += this.vel.y*delta_t; // + 0.5 * this.accel.y * Math.pow(delta_t, 2);
+    this.pos.x += this.isGridDot ? this.vel.x * delta_t * 10000 : this.vel.x * delta_t // + 0.5 * this.accel.x * Math.pow(delta_t, 2)
+    this.pos.y += this.isGridDot ? this.vel.y * delta_t * 10000 : this.vel.y * delta_t // + 0.5 * this.accel.y * Math.pow(delta_t, 2)
   }
 
   distance(other) {
@@ -73,6 +69,8 @@ class Mass {
 
 
 
+
+
 let t = 0;
 let fr = 60;
 const scale = 1 / Math.pow(10, 9);  // in meters
@@ -84,12 +82,14 @@ let universe = [
   Mercury = new Mass("Mercury", 0, -57.91 * Math.pow(10, 9), 2.4397 * Math.pow(10, 6), 3.285 * Math.pow(10, 23)),
   Venus = new Mass("Venus", 0, -108.2 * Math.pow(10, 9), 6.0518 * Math.pow(10, 6), 4.867 * Math.pow(10, 24)),
   Earth = new Mass("Earth", 0, -152 * Math.pow(10, 9), 6.371 * Math.pow(10, 6), 5.972 * Math.pow(10, 24)),
+  // Moon = new Mass("Moon", 0, -152.3844 * Math.pow(10, 9), 1.7371 * Math.pow(10, 6), 7.34767309 * Math.pow(10, 22)),
   Mars = new Mass("Mars", 0, -227.9 * Math.pow(10, 9), 3.3895 * Math.pow(10, 6), 6.39 * Math.pow(10, 23)),
   Jupiter = new Mass("Jupiter", 0, -778.5 * Math.pow(10, 9), 69.911 * Math.pow(10, 6), 1.898 * Math.pow(10, 27))
 ];
 Mercury.vel.x = 47400;
 Venus.vel.x = 35000;
 Earth.vel.x = 30000;
+// Moon.vel.x = 31000;
 Mars.vel.x = 24000;
 Jupiter.vel.x = 13100;
 
@@ -106,6 +106,14 @@ function draw() {
   background(230);
   noStroke();
 
+  let grid = createGrid()
+  // Update forces
+  for (var key in grid) { grid[key].updateForce() }
+  // Update positions
+  for (var key in grid) { grid[key].updatePosition() }
+  // Draw grid
+  for (var key in grid) { grid[key].draw() }
+
   let c = color('yellow');
   fill(c);
   Sun.draw();
@@ -120,7 +128,11 @@ function draw() {
 
   c = color('blue');
   fill(c);
-  Earth.draw(displayText = true);
+  Earth.draw();
+
+  // c = color('grey');
+  // fill(c);
+  // Moon.draw();
 
   c = color('red');
   fill(c);
@@ -137,4 +149,34 @@ function draw() {
   for (var key in universe) { universe[key].updateForce(); }
   // Update positions
   for (var key in universe) { universe[key].updatePosition(); }
+}
+
+
+function createGrid() {
+  let grid = []
+
+  for (var i = -dim/2; i < dim/2; i+=150) {
+    for (var j = -dim/2; j < dim/2; j+=5) {
+      p1 = new Mass(
+        name = `p1_${i}_${j}`,
+        x = i * Math.pow(10, 9),
+        y = j * Math.pow(10, 9),
+        radius = 1,
+        mass = 1.989 * Math.pow(10, 30),
+        isGridDot = true
+      )
+      p2 = new Mass(
+        name = `p2_${j}_${i}`,
+        x = j * Math.pow(10, 9),
+        y = i * Math.pow(10, 9),
+        radius = 1,
+        mass = 1.989 * Math.pow(10, 30),
+        isGridDot = true
+      )
+      grid.push(p1)
+      grid.push(p2)
+    }
+  }
+
+  return grid
 }
